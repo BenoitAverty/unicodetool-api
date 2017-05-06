a33f8f8fb8900a1c751045b1d42d9b98a4316cd1
Benoit Averty <benoit.averty@gmail.com>
2017-05-06 23:21:08 +0200

    chore(deploy): Prepare for deployment

--- src/main/java/org/unicodetool/UnicodetoolServerApplication.java
+++ src/main/java/org/unicodetool/UnicodetoolServerApplication.java
@@ -4,11 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 @SpringBootApplication
 @EnableCaching
 public class UnicodetoolServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UnicodetoolServerApplication.class, args);
+		SpringApplication app = new SpringApplication(UnicodetoolServerApplication.class);
+		Map<String, Object> defaultProps = new HashMap<>();
+		defaultProps.put("spring.profiles.active", "dev");
+		app.setDefaultProperties(defaultProps);
+
+		app.run(args);
 	}
 }
