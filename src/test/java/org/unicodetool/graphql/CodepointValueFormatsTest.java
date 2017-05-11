87358600006d2e1ea62b6e0ed6b4421a745dfcb7
Benoit Averty <benoit.averty@gmail.com>
2017-05-12 00:40:01 +0200

    feat(codepoint): Add the decimal value of the codepoint in schema

---
+++ src/test/java/org/unicodetool/graphql/CodepointValueFormatsTest.java
@@ -0,0 +1,30 @@
+package org.unicodetool.graphql;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.unicodetool.graphql.schema.CodepointValue;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test the different ways to diaply the value of a codepoint
+ */
+public class CodepointValueFormatsTest {
+
+    @Test
+    @DisplayName("Format Codepoint Value as Decimal Integer")
+    public void testAsDecimal() {
+        final Integer expected = 41;
+        final CodepointValue input = CodepointValue.of("0029");
+
+        assertEquals(input.inDecimalFormat(), expected);
+    }
+
+    @Test
+    @DisplayName("Format Codepoint Value as unicode standard")
+    public void testAsUnicode() {
+        final String expected = "U+0029";
+        final CodepointValue input = CodepointValue.of(41);
+
+        assertEquals(input.inUnicodeFormat(), expected);
+    }
+}
