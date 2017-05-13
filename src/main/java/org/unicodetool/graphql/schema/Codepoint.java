1049f51911a4b283813d19e366b3200e18438960
Benoit Averty <benoit.averty@gmail.com>
2017-05-13 11:20:15 +0200

    refactor: Use lombok in Codepoint class

--- src/main/java/org/unicodetool/graphql/schema/Codepoint.java
+++ src/main/java/org/unicodetool/graphql/schema/Codepoint.java
@@ -1,31 +1,19 @@
 package org.unicodetool.graphql.schema;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * Common behaviour between all codepoint types. Corresponds to the Codepoint Interface in the schema.
  */
 @AllArgsConstructor
+@Getter
 public abstract class Codepoint {
     private CodepointValue value;
     private String name;
-
     private Properties properties;
-
-    public CodepointValue getValue() {
-        return value;
-    }
 
     public Integer getDecimalValue() {
         return value.inDecimalFormat();
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public Properties getProperties() {
-        return properties;
-    }
-
 }
